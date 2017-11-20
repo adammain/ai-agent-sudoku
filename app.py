0cc4704eded1c4e489957d4c64f307a61ff271c2
amtruenorth <northforth@me.com>
2017-11-20 07:03:16 -0500

    feat: initialize main program functions

--- app.py
+++ app.py
@@ -16,3 +16,17 @@ def assign_value(values, box, value):
     if len(value) == 1:
         assignments.append(values.copy())
     return values
+
+
+if __name__ == '__main__':
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(solve(diag_sudoku_grid))
+
+    try:
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
+
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
